da4e97aa91c4ffc0c2e65b828eed4350c7b0b52b
Gonçalo Pereira <goncalo.valpneus@hotmail.com>
2019-03-15 17:24:07 +0000

    Aula 3(inicio)

---
+++ AULA3/cal_fp03/cmake-build-debug/CMakeFiles/cal_fp03.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/goncalo/FEUP/CAL/AULA3/cal_fp03/src/NearestPoints.cpp" "/home/goncalo/FEUP/CAL/AULA3/cal_fp03/cmake-build-debug/CMakeFiles/cal_fp03.dir/src/NearestPoints.cpp.o"
+  "/home/goncalo/FEUP/CAL/AULA3/cal_fp03/src/Point.cpp" "/home/goncalo/FEUP/CAL/AULA3/cal_fp03/cmake-build-debug/CMakeFiles/cal_fp03.dir/src/Point.cpp.o"
+  "/home/goncalo/FEUP/CAL/AULA3/cal_fp03/src/Test.cpp" "/home/goncalo/FEUP/CAL/AULA3/cal_fp03/cmake-build-debug/CMakeFiles/cal_fp03.dir/src/Test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/cute"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AULA3/cal_fp03/cmake-build-debug/CMakeFiles/cal_fp03.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cal_fp03.dir/src/NearestPoints.cpp.o"
+  "CMakeFiles/cal_fp03.dir/src/Point.cpp.o"
+  "CMakeFiles/cal_fp03.dir/src/Test.cpp.o"
+  "cal_fp03.pdb"
+  "cal_fp03"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cal_fp03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
